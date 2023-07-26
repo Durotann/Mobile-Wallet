7e742387b028065e326ac7993251b146afe976e7
Rashid <rashid_rashidov_04@mail.ru>
2023-07-27 00:44:26 +0300

    s

--- walletapp/lib/domain/view/login_screen.dart
+++ walletapp/lib/domain/view/login_screen.dart
@@ -16,25 +16,6 @@ class LogScreen extends StatefulWidget {
 }
 
 class _LogScreenState extends State<LogScreen> {
-  final TextEditingController _controllerEmail = TextEditingController();
-  final TextEditingController _controllerPassword = TextEditingController();
-
-  Future<void> signInWithEmailAndPassword() async {
-    try {
-      await Auth().signInWithEmailAndPassword(
-          email: _controllerEmail.text, password: _controllerPassword.text);
-    } on FirebaseAuthException catch (e) {}
-    ;
-  }
-
-  Future<void> createUserWithEmailAndPassword() async {
-    try {
-      await Auth().createUserWithEmailAndPassword(
-          email: _controllerEmail.text, password: _controllerPassword.text);
-    } on FirebaseAuthException catch (e) {}
-    ;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,162 +57,7 @@ class _LogScreenState extends State<LogScreen> {
             SizedBox(
               height: Adaptive.h(2),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Container(
-                height: Adaptive.h(24),
-                width: double.infinity,
-                decoration: BoxDecoration(color: Color.fromRGBO(40, 51, 63, 1)),
-                child: Column(
-                  children: [
-                    TextField(
-                      style: TextStyle(color: Colors.white),
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.left,
-                      controller: _controllerEmail,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color.fromRGBO(47, 60, 80, 1),
-                        hintText: "Email",
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 16, vertical: Adaptive.h(2.5)),
-                        hintStyle: TextStyle(
-                          color: Color.fromRGBO(174, 168, 178, 1),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(123, 97, 255, 1))),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(47, 60, 80, 1))),
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    SizedBox(
-                      height: Adaptive.h(2),
-                    ),
-                    TextField(
-                      style: TextStyle(color: Colors.white),
-                      obscureText: changeVis,
-                      controller: _controllerPassword,
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Color.fromRGBO(47, 60, 80, 1),
-                          hintText: "Password",
-                          contentPadding: EdgeInsets.symmetric(
-                              horizontal: 16, vertical: Adaptive.h(2.5)),
-                          hintStyle: TextStyle(
-                            color: Color.fromRGBO(174, 168, 178, 1),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: BorderSide(
-                                  color: Color.fromRGBO(123, 97, 255, 1))),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: BorderSide(
-                                  color: Color.fromRGBO(47, 60, 80, 1))),
-                          border: OutlineInputBorder(),
-                          suffixIcon: Padding(
-                            padding: const EdgeInsets.only(right: 16.0),
-                            child: IconButton(
-                              enableFeedback: false,
-                              onPressed: () {
-                                setState(() {
-                                  if (changeVis == true) {
-                                    changeVis = false;
-                                    iconcolor = Color.fromRGBO(123, 97, 255, 1);
-                                  } else {
-                                    changeVis = true;
-                                    iconcolor = Color.fromRGBO(47, 60, 80, 1);
-                                  }
-                                });
-                              },
-                              icon: Icon(Icons.remove_red_eye_outlined),
-                              color: iconcolor,
-                            ),
-                          )),
-                    ),
-                    SizedBox(
-                      height: Adaptive.h(1.5),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 20,
-                              height: 20,
-                              child: Checkbox(
-                                activeColor: Color.fromRGBO(123, 97, 255, 1),
-                                value: isChecked,
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    isChecked = value!;
-                                  });
-                                },
-                              ),
-                            ),
-                            SizedBox(
-                              width: Adaptive.h(2),
-                            ),
-                            TextF(
-                              text: "Remember Me",
-                              fsize: 14,
-                              fweight: FontWeight.w400,
-                              color: Colors.white,
-                            ),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: TextF(
-                              color: Color.fromRGBO(123, 97, 255, 1),
-                              text: "Forgot Password ?",
-                              fsize: 14,
-                              fweight: FontWeight.w400),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: Adaptive.h(2),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Container(
-                height: 56,
-                width: double.infinity,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                        backgroundColor: Color.fromRGBO(123, 97, 255, 1)),
-                    onPressed: () {
-                      createUserWithEmailAndPassword();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()),
-                      );
-                    },
-                    child: Center(
-                      child: TextF(
-                        color: Colors.white,
-                        text: "Log In",
-                        fsize: 18,
-                        fweight: FontWeight.w600,
-                      ),
-                    )),
-              ),
-            ),
+            TextFl(),
             SizedBox(
               height: Adaptive.h(4),
             ),
@@ -253,6 +79,15 @@ class _LogScreenState extends State<LogScreen> {
                         text: "Or continue with",
                         fsize: 12,
                         fweight: FontWeight.w400)),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 24.0),
+                    child: Divider(
+                      color: Color.fromRGBO(75, 87, 107, 1),
+                      thickness: 1,
+                    ),
+                  ),
+                ),
               ],
             ),
             SizedBox(
--- walletapp/lib/domain/widgets/textfield.dart
+++ walletapp/lib/domain/widgets/textfield.dart
@@ -4,6 +4,7 @@ import 'package:walletapp/domain/widgets/text.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
 import '../service/auth.dart';
+import '../view/home_screen.dart';
 import '../viewModel/containts.dart';
 
 class TextFl extends StatefulWidget {
@@ -14,8 +15,182 @@ class TextFl extends StatefulWidget {
 }
 
 class _TextFlState extends State<TextFl> {
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+          email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {}
+    ;
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+          email: _controllerEmail.text, password: _controllerPassword.text);
+    } on FirebaseAuthException catch (e) {}
+    ;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Container(
+        height: Adaptive.h(31),
+        width: double.infinity,
+        decoration: BoxDecoration(color: Color.fromRGBO(40, 51, 63, 1)),
+        child: Column(
+          children: [
+            TextField(
+              style: TextStyle(color: Colors.white),
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.left,
+              controller: _controllerEmail,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromRGBO(47, 60, 80, 1),
+                hintText: "Email",
+                contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16, vertical: Adaptive.h(2.5)),
+                hintStyle: TextStyle(
+                  color: Color.fromRGBO(174, 168, 178, 1),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide:
+                        BorderSide(color: Color.fromRGBO(123, 97, 255, 1))),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide:
+                        BorderSide(color: Color.fromRGBO(47, 60, 80, 1))),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: Adaptive.h(2),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              obscureText: changeVis,
+              controller: _controllerPassword,
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(47, 60, 80, 1),
+                  hintText: "Password",
+                  contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16, vertical: Adaptive.h(2.5)),
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(174, 168, 178, 1),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide:
+                          BorderSide(color: Color.fromRGBO(123, 97, 255, 1))),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide:
+                          BorderSide(color: Color.fromRGBO(47, 60, 80, 1))),
+                  border: OutlineInputBorder(),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: IconButton(
+                      enableFeedback: false,
+                      onPressed: () {
+                        setState(() {
+                          if (changeVis == true) {
+                            changeVis = false;
+                            iconcolor = Color.fromRGBO(123, 97, 255, 1);
+                          } else {
+                            changeVis = true;
+                            iconcolor = Color.fromRGBO(47, 60, 80, 1);
+                          }
+                        });
+                      },
+                      icon: Icon(Icons.remove_red_eye_outlined),
+                      color: iconcolor,
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: Adaptive.h(1.5),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Checkbox(
+                        activeColor: Color.fromRGBO(123, 97, 255, 1),
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value!;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: Adaptive.h(2),
+                    ),
+                    TextF(
+                      text: "Remember Me",
+                      fsize: 14,
+                      fweight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: TextF(
+                      color: Color.fromRGBO(123, 97, 255, 1),
+                      text: "Forgot Password ?",
+                      fsize: 14,
+                      fweight: FontWeight.w400),
+                )
+              ],
+            ),
+            SizedBox(
+              height: Adaptive.h(2),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Container(
+                height: 56,
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        backgroundColor: Color.fromRGBO(123, 97, 255, 1)),
+                    onPressed: () {
+                      createUserWithEmailAndPassword();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                      );
+                    },
+                    child: Center(
+                      child: TextF(
+                        color: Colors.white,
+                        text: "Log In",
+                        fsize: 18,
+                        fweight: FontWeight.w600,
+                      ),
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
